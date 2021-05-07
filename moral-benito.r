4d3ea4bde9bffed9705685c775e62bf0134e8909
Mateusz Wyszyński <Mateusz@Mateuszs-MacBook-Pro.local>
2021-05-07 18:21:12 +0200

    code: move myhess function to stat_functions script

--- moral-benito.r
+++ moral-benito.r
@@ -132,40 +132,6 @@ for (regressors_subset in regressors_subsets) {
   # theta returns optimized parameters, fout is the value of the function lik at the maximum
   # we now compute model-specific standard errors @
   
-    #-----------------------------------------------------------------#
-    #                        HESSIAN FUNCTION                         #
-    #                        -----------------                        #
-    #               this procedure takes as input a number            #
-    #               computes the kxk (2-sided) hessian matrix         #
-    #-----------------------------------------------------------------#
-    
-   myhess<-function(lik,theta) {
-    x0<-theta
-    k=nrow(x0) # x0 is theta, our model-specific optimized parameters #
-    hessi=zeros(k,k)
-    h=1e-3
-    
-    for (jc in 1:k) {
-      for (jr in 1:jc) {
-        x1=x0
-        x2=x0
-        x3=x0
-        x4=x0
-        x1[jr]=x0[jr]+h
-        x1[jc]=x1[jc]+h
-        x2[jr]=x0[jr]+h    
-        x2[jc]=x2[jc]-h
-        x3[jr]=x0[jr]-h
-        x3[jc]=x3[jc]+h
-        x4[jr]=x0[jr]-h
-        x4[jc]=x4[jc]-h
-        hessi[jr,jc]=(lik(x1)-lik(x2)-lik(x3)+lik(x4))/(4*h^2) # the second symmetric derivative has different formula #
-        hessi[jc,jr]=hessi[jr,jc]
-      }
-    }
-    return(hessi)
-  } #for some reason, negative values are produced
-  
   he=myhess(lik,theta)
   #he=hessian(lik,theta) #alternative methods
   #hess=(fdHess(theta,lik))
