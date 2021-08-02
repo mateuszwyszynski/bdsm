343d9738dd4565f5f661acd9a5b79f4bd38a7bd2
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2021-08-02 16:31:58 +0200

    test: create one test dataframe for multiple tests

--- tests/testthat/test-SEM_likelihood.R
+++ tests/testthat/test-SEM_likelihood.R
@@ -1,13 +1,13 @@
+test_df <- tibble(
+  entities = rep(1:3, 5),
+  times = rep(seq(1960, 2000, 10), each = 3),
+  dep_var = 101:115,
+  a = 2:16,
+  b = 3:17
+)
+
 test_that(paste("SEM_dep_var_matrix uses all timestamps if start_time argument",
                 "is not given"), {
-  df <- tibble(
-    entities = rep(1:3, 5),
-    times = rep(seq(1960, 2000, 10), each = 3),
-    dep_var = 101:115,
-    a = 2:16,
-    b = 3:17
-  )
-
   m_expected_data <- c(
     101, 104, 107, 110, 113,
     102, 105, 108, 111, 114,
@@ -15,7 +15,7 @@ test_that(paste("SEM_dep_var_matrix uses all timestamps if start_time argument",
   )
   m_expected <- matrix(m_expected_data, nrow = 3, byrow = TRUE)
 
-  m <- SEM_dep_var_matrix(df = df, timestamp_col = times,
+  m <- SEM_dep_var_matrix(df = test_df, timestamp_col = times,
                           entity_col = entities, dep_var_col = dep_var)
 
   expect_equal(m, m_expected, ignore_attr = TRUE)
