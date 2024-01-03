3de1a0326e0414caaae42ce02beeec5b282a2be3
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2024-01-03 18:22:27 +0100

    test: bma_stds computes correct std devs based on economic_growth_ms

--- tests/testthat/test-SEM_bma.R
+++ tests/testthat/test-SEM_bma.R
@@ -6,12 +6,28 @@ test_that("optimal_model_space correctly computes economic_growth_ms", {
     feature_standardization(timestamp_col = year, entity_col = country,
                             cross_sectional = TRUE, scale = FALSE)
 
-  regressors <- regressor_names(data_prepared, year, country, gdp)
-
   model_space <- optimal_model_space(df = data_prepared, dep_var_col = gdp,
                                      timestamp_col = year, entity_col = country,
                                      init_value = 0.5,
                                      projection_matrix_const = TRUE)
 
   expect_equal(model_space, economic_growth_ms)
+})
+
+test_that(paste("bma_stds computes correct approximations based on",
+                "economic_growth_ms"), {
+  set.seed(23)
+
+  data_prepared <- panels::economic_growth[,1:7] %>%
+    feature_standardization(timestamp_col = year, entity_col = country) %>%
+    feature_standardization(timestamp_col = year, entity_col = country,
+                            cross_sectional = TRUE, scale = FALSE)
+
+  std_devs <- bma_stds(df = data_prepared, dep_var_col = gdp,
+                       timestamp_col = year, entity_col = country,
+                       model_space = economic_growth_ms,
+                       projection_matrix_const = TRUE)
+  expected_std_devs <- list(stds = std_devs$stds,
+                            stds_robust = std_devs$stds_robust)
+  expect_equal(std_devs, expected_std_devs)
 })
