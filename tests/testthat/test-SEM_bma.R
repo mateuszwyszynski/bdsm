230c1ccf421bde06d281f505f5750bbb0d251c34
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2024-01-03 22:56:46 +0100

    refactor: separated model space functionality from bma summary

--- tests/testthat/test-SEM_bma.R
+++ tests/testthat/test-SEM_bma.R
@@ -1,19 +1,3 @@
-test_that("optimal_model_space correctly computes economic_growth_ms", {
-  set.seed(23)
-
-  data_prepared <- panels::economic_growth[,1:7] %>%
-    feature_standardization(timestamp_col = year, entity_col = country) %>%
-    feature_standardization(timestamp_col = year, entity_col = country,
-                            cross_sectional = TRUE, scale = FALSE)
-
-  model_space <- optimal_model_space(df = data_prepared, dep_var_col = gdp,
-                                     timestamp_col = year, entity_col = country,
-                                     init_value = 0.5,
-                                     projection_matrix_const = TRUE)
-
-  expect_equal(model_space, economic_growth_ms)
-})
-
 test_that(paste("bma_stds computes correct approximations based on",
                 "economic_growth_ms"), {
   set.seed(23)
