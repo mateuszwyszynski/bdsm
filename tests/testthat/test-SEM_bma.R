e51abaea8378bd65255c2e3edd3a5cb79be1f6d0
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2024-01-06 22:17:23 +0100

    feat: function to compute likelihoods summary (not only std devs)

--- tests/testthat/test-SEM_bma.R
+++ tests/testthat/test-SEM_bma.R
@@ -15,3 +15,20 @@ test_that(paste("bma_stds computes correct approximations based on",
                             stds_robust = std_devs$stds_robust)
   expect_equal(std_devs, expected_std_devs)
 })
+
+test_that(paste("likelihoods_summary computes correct approximations of",
+                "standard deviations based on economic_growth_ms"), {
+  set.seed(23)
+
+  data_prepared <- panels::economic_growth[,1:7] %>%
+    feature_standardization(timestamp_col = year, entity_col = country) %>%
+    feature_standardization(timestamp_col = year, entity_col = country,
+                            cross_sectional = TRUE, scale = FALSE)
+
+  lik_info <- likelihoods_summary(df = data_prepared, dep_var_col = gdp,
+                                  timestamp_col = year, entity_col = country,
+                                  model_space = economic_growth_ms,
+                                  projection_matrix_const = TRUE)
+  expected_lik_info <- rbind(economic_growth_stds, economic_growth_stds_robust)
+  expect_equal(lik_info[-1,], expected_lik_info)
+})
