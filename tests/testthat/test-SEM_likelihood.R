3a10b0a0b5c404003be0e3d7a41ddd665726380e
Marcin Dubel <marcindubel@gmail.com>
2024-03-26 09:35:16 +0100

    test: extract data preparation steps to helper script

--- tests/testthat/test-SEM_likelihood.R
+++ tests/testthat/test-SEM_likelihood.R
@@ -9,23 +9,6 @@
 # min      lq       mean.    median   uq       max        neval
 # 25.80372 26.05294 27.14582 26.22264 27.25252 36.02822   100
 
-
-generate_test_feature_standard_data <- function(cross_sectional = FALSE, scale = TRUE) {
-  df <- data.frame(
-    entities = rep(1:4, 5),
-    times = rep(seq(1960, 2000, 10), each = 4),
-    dep_var = stats::rnorm(20), a = stats::rnorm(20), b = stats::rnorm(20)
-  )
-
-  feature_standardization(
-    df,
-    timestamp_col = times,
-    entity_col = entities,
-    cross_sectional,
-    scale
-  )
-}
-
 test_that("SEM likelihood is calculated correctly for default feature standardization parameters", {
   set.seed(1)
   sem_value <- SEM_likelihood(
@@ -66,14 +49,3 @@ test_that("SEM likelihood is calculated correctly for cross_sectional FALSE and 
   )
   expect_equal(sem_value, NaN)
 })
-
-
-
-
-
-# Check those NaN
-microbenchmark(SEM_likelihood(
-  0.5,
-  generate_test_feature_standard_data(),
-  times, entities, dep_var
-))
