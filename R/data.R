804d58d7e792cc575bf65199bca1e671b39e2bd0
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-04-11 19:02:35 +0200

    feat: remove outdated model spaces for the full model

--- R/data.R
+++ R/data.R
@@ -34,38 +34,6 @@
 #' A double matrix with 51 rows and 16 columns.
 "economic_growth_ms"
 
-#' Full Model Space with Varying Projection Matrix
-#'
-#' A matrix representing the model space built using all regressors from
-#' the \code{economic_growth} dataset. Therefore the model space contains
-#' \code{2^9 = 512} models (columns). This model space generates Posterior
-#' Inclusion Probabilities which are consistent with the results presented by
-#' Moral-Benito. The original results were approximated up to the 4th decimal
-#' place. The results obtained using this model space lead to exactly the same
-#' approximations. A different projection matrix is used for each model.
-#'
-#' @format ## `economic_growth_ms_full_proj_var`
-#' A double matrix with 106 rows and 512 columns.
-"economic_growth_ms_full_proj_var"
-
-#' Full Model Space with Constant Projection Matrix
-#'
-#' A matrix representing the model space built using all regressors from
-#' the \code{economic_growth} dataset. Therefore the model space contains
-#' \code{2^9 = 512} models (columns). The same projection matrix is used for
-#' each model.
-#'
-#' TODO: to avoid NaNs when computing estimates of standard deviations, the step
-#' size in the hessian function has to be increased to 1e-2. This is most likely
-#' cause by the fact that the likelihood values are much closer to each other
-#' after the correction for the projection matrix is introduced. Hence we have
-#' to either increase the relative tolerance of the optimization algorithm or
-#' loosen the precision when computing approximate hessian.
-#'
-#' @format ## `economic_growth_ms_full_proj_const`
-#' A double matrix with 106 rows and 512 columns.
-"economic_growth_ms_full_proj_const"
-
 #' Example Approximate Likelihoods Summary based on Model Space
 #'
 #' A matrix representing the summary of likelihoods computed with
