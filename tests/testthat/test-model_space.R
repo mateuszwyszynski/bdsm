fe2c5cb29ec5b76e8bc96a73240a5b92bab6cc1b
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-04-13 09:59:47 +0200

    refactor: merge model space params functions into parent model space script

--- tests/testthat/test-model_space.R
+++ tests/testthat/test-model_space.R
@@ -1,3 +1,30 @@
+test_that("optim_model_space_params correctly computes small_economic_growth_ms", {
+  skip_on_os(c("windows", "linux"))
+  skip_on_cran()
+  set.seed(23)
+
+  data_prepared <- bdsm::economic_growth[,1:6] %>%
+    bdsm::feature_standardization(
+      excluded_cols = c(country, year, gdp)
+    ) %>%
+    bdsm::feature_standardization(
+      group_by_col  = year,
+      excluded_cols = country,
+      scale         = FALSE
+    )
+
+  model_space <- optim_model_space_params(
+    df            = data_prepared,
+    dep_var_col   = gdp,
+    timestamp_col = year,
+    entity_col    = country,
+    init_value    = 0.5
+  )
+
+  expect_equal(model_space, small_model_space[[1]])
+})
+
+
 test_that(paste("model_space computes correct model_space list"), {
 
   data_prepared <- bdsm::economic_growth[,1:5] %>%
@@ -23,5 +50,3 @@ test_that(paste("model_space computes correct model_space list"), {
   expect_equal(class(for_bma[[1]]), c("matrix","array"))
   expect_equal(class(for_bma[[2]]), c("matrix","array"))
 })
-
-
