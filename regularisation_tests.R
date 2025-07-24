d5827b9c7e2008615fa8d06678d6961bf69b094f
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-07-24 17:27:01 +0200

    docs: general formula for the necessary condition for the H matrix to be symmetric positive definite

--- regularisation_tests.R
+++ regularisation_tests.R
@@ -6,26 +6,50 @@ generate_test_data <- function(n_entities, n_periods) {
     a = round(stats::rnorm(n_entities * n_periods), 5),
     b = round(stats::rnorm(n_entities * n_periods), 5),
     c = round(stats::rnorm(n_entities * n_periods), 5),
+    d = round(stats::rnorm(n_entities * n_periods), 5),
+    e = round(stats::rnorm(n_entities * n_periods), 5)
   ),
   class = "data.frame", row.names = c(NA, -n_entities * n_periods))
 }
 
-set.seed(1)
-# After testing different n_entities and n_periods here,
-# and then evaluating the sem likelihood further in the code below,
-# I believe that:
+# set.seed(1)
+# One can generate a random data with a different number of regressors using the code below.
+# Then we can test how sem_likelihood behaves.
+# If you add some debugging messages, you can print out the eigenvalues of the matrix H and check their number.
 #
-# 1. There are awlays (n_periods - 2)*(number of regressors) eigenvalues in the matrix H_scaled
-# 2. After the first n_entities - 2 eigenvalues, the rest will be zero.
+# I've done multiple such tests. After going through them, I arrived at the following conclusions:
 #
-# So to have a working model we need:
+# 1. The symmetric matrix H awlays has dimension (n_periods - 2)*(number of regressors),
+# so it will have this number of eigenvalues
+# 2. The rank of H is bounded by rank(H) = rank(M^T P M) = rank( M^T P^{1/2} P^{1/2} M) = rank (P^{1/2} M) <= min(rank(P), rank(M))
+# where:
+#  - the P^{1/2} exists because P is a projection matrix, i.e., symmetric semi positive definite (all eigenvalues >= 0)
+#  - we have rank(X^T X) = rank(X)
+#  - we have rank(AB) <= min(rank(A), rank(B))
 #
-# n_entities - 2 >= (n_periods - 2)*(number of regressors)
+# Combining 1. and 2. together leads to the following necessary condition for the H matrix to be symmetric positive definite
+# (all eigenvalues > 0):
 #
-# because otherwise we end up with some of the eigenvalues automatically zero.
-# Not sure yet why, but this seems to be the case
+# n_entities - n_regressors - 1 >= (n_periods - 2)*n_regressors
+#
+# where the RHS is simply the number of eigenvalues the H has, and the LHS is the
+#
+# rank(P) = rank(I) - rank(Z) = N - (n_regressors + 1)
+#
+# If one wants to express the condition as a bound on the number of entities needed for the approach to work:
+#
+# n_entities >= (n_periods - 1)*n_regressors + 1 (**)
+#
+# IMPORTANT: For some reason in my experiments with randomly generated data I always need to satisfy a strict inequality in (**).
+# I'm not sure yet what is the reason for that.
+#
+# Example:
+# n_regressors = 5
+# n_periods = 12
+#
+# so 11*5 + 1 = 56 entities are necessary to even have a chance of having non-zero eigenvalues in H.
 
-df_test <- generate_test_data(n_entities = 25, n_periods = 10)
+df_test <- generate_test_data(n_entities = 56, n_periods = 12)
 
 matrices_shared_across_models <- df_test %>%
   matrices_from_df(timestamp_col = times,
@@ -46,7 +70,8 @@ df_test_prepared <- df_test %>%
 sem_value <- sem_likelihood(
   0.5,
   df_test_prepared,
-  times, entities, dep_var
+  times, entities, dep_var,
+  lin_related_regressors = c("a","b","c","d","e")
 )
 
 X <- cbind(
