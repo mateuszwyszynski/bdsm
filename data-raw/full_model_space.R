f57ae7da7976cd450c69537af9e0539875f0878a
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-09-22 23:53:26 +0200

    fix: library(bdsm) instead of devtools::load_all()

--- data-raw/full_model_space.R
+++ data-raw/full_model_space.R
@@ -22,7 +22,7 @@ if (is.na(cores)) {
   cores <- min(cores, detectCores())
 }
 cl <- makeCluster(cores)
-clusterEvalQ(cl, devtools::load_all())
+clusterEvalQ(cl, library(bdsm))
 
 full_model_space <- optim_model_space(
   df            = data_prepared,
