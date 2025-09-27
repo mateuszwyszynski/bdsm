d1e444a87e47e81039a7c005588b1e6a589e408e
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-09-27 20:36:06 +0200

    feat: use devtools::load_all() when regenarating data

--- data-raw/full_model_space.R
+++ data-raw/full_model_space.R
@@ -22,7 +22,7 @@ if (is.na(cores)) {
   cores <- min(cores, detectCores())
 }
 cl <- makeCluster(cores)
-clusterEvalQ(cl, library(bdsm))
+clusterEvalQ(cl, devtools::load_all())
 
 full_model_space <- optim_model_space(
   df            = data_prepared,
